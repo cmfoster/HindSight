9db39b580487332542f284cf89772e0d8333fa21
cmfoster <curtismfoster@gmail.com>
2012-01-13 11:30:06 -0600

    Post form added, still need to change colors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,35 @@
 class PostsController < ApplicationController
   def show
+    @post = Post.find_by_id(params[:id])
+    if @post
+      respond_to do |format|
+	format.js
+      end
+    end
   end
 
-  def edit
+  def update
+    @post = Post.find_by_id(params[:id])
+    if @post
+      respond_to do |format|
+	if @post.update_attributes(params[:post])
+	  format.js 
+	else
+	  redirect_to root
+	end
+      end
+    end
   end
   
   def create
     @post = Post.create(params[:post])
     @post.user_id = current_user.id
-    @post.save!
+    @post.author = current_user.name
+    @post.save
     respond_to do |format|
+      format.html {redirect_to root_path }
       format.js
     end
   end
-
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120112012155) do
+ActiveRecord::Schema.define(:version => 20120113053547) do
 
   create_table "hinds", :force => true do |t|
     t.string   "subject"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20120112012155) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "title"
+    t.string   "author"
   end
 
   create_table "users", :force => true do |t|
